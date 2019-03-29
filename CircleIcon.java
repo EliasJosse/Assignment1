589d86d66e19581efb0c7afd5f5c6521357b08f7
Elias <eliasjosse@gmail.com>
2019-03-29 21:28:16 +0100

    14

---
+++ CircleIcon.java
@@ -0,0 +1,67 @@
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import javax.swing.Icon;
+
+public abstract class CircleIcon implements Icon{
+	
+	private int size;
+	
+	public CircleIcon(int aSize){
+		
+		size = aSize;
+		
+	}
+	
+	public interface Icon{
+		
+		int getIconWidth();
+		int getIconHeight();
+		void paintIcon(Component a, Graphics g, int x, int y, Color c);
+		
+	}
+	
+	public int getIconWidth(){
+		
+		return size;
+		
+	}
+	
+	public int getIconHeight(){
+		
+		return size;
+		
+	}
+	
+	public void paintIcon(Component a, Graphics g, int x, int y, Color c){
+
+		Graphics2D g2 = (Graphics2D) g;
+		Ellipse2D.Double circle = new Ellipse2D.Double(x, y, 1, 1);
+		g2.setColor(c);
+		g2.fill(circle);
+		
+	}
+	
+	/*public void repaint(CircleIcon a, Color c){
+		
+		if(c == Color.RED){
+			
+			a.paintIcon(Color.RED);
+			
+		}
+		else if(c == Color.GREEN){
+		
+			a.paintIcon(Color.GREEN);
+			
+		}
+		else{
+			
+			a.paintIcon(Color.BLUE);
+			
+		}
+		
+	}*/
+
+}
---
+++ Colors.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+
+public class Colors {
+	
+	public Colors(){
+	
+		JFrame frame = new JFrame();
+	
+		//frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+	
+		JButton red = new JButton("RED");
+		JButton green = new JButton("GREEN");
+		JButton blue = new JButton("BLUE");
+		
+		JLabel label = new JLabel("hej"); //for the circle
+		//Graphics2D g = null;
+		//CircleIcon redCircle = null;
+		//redCircle.paintIcon(label, g, 0, 0, Color.RED);
+		//JLabel label = new JLabel(redCircle);
+		
+		frame.add(red);
+		frame.add(green);
+		frame.add(blue);
+		frame.add(label);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		label.setBounds(150, 50, 200, 200); //For the circle
+		red.setBounds(100, 300, 100, 50);
+		green.setBounds(200, 300, 100, 50);
+		blue.setBounds(300, 300, 100, 50);
+		frame.setSize(500, 500);
+		//frame.pack();
+		frame.setLayout(null);
+		frame.setVisible(true);
+	
+	}
+	
+	public static void main(String[] args){
+		
+		Colors a = new Colors();
+		
+	}
+
+}
+
